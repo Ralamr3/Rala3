f34f30005f8bf62c5fdbe94f926af2dc576ee60b
Ralamr3 <Raghad1999rr@icloud.com>
2024-03-28 03:50:02 +0300

    query database table and use ORM to filter and query data in views.py

--- prog_platform/prog_platform_app/views.py
+++ prog_platform/prog_platform_app/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
+from .models import ProgrammingLanguage
 
 def index(request):
     # this view returns the main page of the programming platform
     return render(request, 'prog_platform_module/index.html')
 
 def language_list(request):
-    # this view returns a list of programming languages
-    return render(request, 'prog_platform_module/languageList.html',{'languages':__getLanguages()})
+    languages = ProgrammingLanguage.objects.all().order_by('name')
+    return render(request, 'prog_platform_module/languageList.html', {'languages': languages})
+
+
+
 def language_details(request, lang_id):
     
     language1 = {'id': 1, 'name': 'Python', 'creator': 'Guido van Rossum'}
@@ -21,24 +25,16 @@ def language_details(request, lang_id):
     context = {'language': targetLanguage} # 'language' is the variable name accessible by the template
     return render(request, 'prog_platform_module/languageDetails.html', context)
 
-def __getLanguages():
-    return [
-        {'id': 1, 'name': 'Python', 'creator': 'Guido van Rossum', 'release_year': 1991, 'paradigm': 'Object-oriented, Imperative, Functional', 'typical_use': 'Web development, Data Science, Automation'},
-        {'id': 2, 'name': 'JavaScript', 'creator': 'Brendan Eich', 'release_year': 1995, 'paradigm': 'Event-driven, Functional', 'typical_use': 'Web development, Front-end scripting'},
-        {'id': 3, 'name': 'Java', 'creator': 'James Gosling', 'release_year': 1995, 'paradigm': 'Object-oriented', 'typical_use': 'Enterprise applications, Android apps'},
-        {'id': 4, 'name': 'C#', 'creator': 'Microsoft', 'release_year': 2000, 'paradigm': 'Object-oriented', 'typical_use': 'Desktop applications, Game development'},
-        {'id': 5, 'name': 'Ruby', 'creator': 'Yukihiro Matsumoto', 'release_year': 1995, 'paradigm': 'Object-oriented', 'typical_use': 'Web applications, Scripting'}
-    ]
 
 def filter_language(request):
-    all_languages = __getLanguages()
     search_name = request.GET.get('search_name', '')
     search_paradigm = request.GET.get('paradigm', '')
 
+    languages = ProgrammingLanguage.objects.all()
     if search_name:
-        all_languages = [lang for lang in all_languages if search_name.lower() in lang['name'].lower()]
+        languages = languages.filter(name__icontains=search_name)
     if search_paradigm:
-        all_languages = [lang for lang in all_languages if search_paradigm in lang['paradigm']]
+        languages = languages.filter(paradigm__icontains=search_paradigm)
 
-    context = {'languages': all_languages}
-    return render(request, 'prog_platform_module/languageList.html', context)
+    languages = languages.order_by('name')
+    return render(request, 'prog_platform_module/languageList.html', {'languages': languages})
